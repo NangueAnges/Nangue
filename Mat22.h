826aaa874f35feefd94635638b104e9f65db7366
NangueAnges <nangueanges@gmail.com>
2024-10-27 00:27:28 +0100

    Create Mat22.h

---
+++ Mat22.h
@@ -0,0 +1,17 @@
+void matrice(float mat[2][2]);
+
+void afficherMatrice(float mat[2][2]);
+
+void somme(float mat1[2][2], float mat2[2][2], float som[2][2]);
+
+void soustraction(float mat1[2][2], float mat2[2][2], float sous[2][2]);
+
+void produit(float mat1[2][2], float mat2[2][2], float prod[2][2]);
+
+float Determinant( float mat[2][2]);
+
+bool Inverse(float mat[2][2], float invers[2][2]);
+
+float Trace(float mat[2][2]);
+
+void Transposee(float mat[2][2], float trans[2][2]);
