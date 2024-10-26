b8fa2ca5895ebe14614144fd81b7f3ca84f40bc6
NangueAnges <nangueanges@gmail.com>
2024-10-27 00:31:35 +0100

    Create Mat33.h

---
+++ Mat33.h
@@ -0,0 +1,17 @@
+void matrice(float mat[3][3]);
+
+void afficherMatrice(float mat[3][3]);
+
+void somme(float mat1[3][3], float mat2[3][3], float som[3][3]);
+
+void soustraction(float mat1[3][3], float mat2[3][3], float sous[3][3]);
+
+void produit(float mat1[3][3], float mat2[3][3], float prod[3][3]);
+
+float Determinant( float mat[3][3]);
+
+bool Inverse(float mat[3][3], float invers[3][3]);
+
+float Trace(float mat[3][3]);
+
+void Transposee(float mat[3][3], float trans[3][3]);
